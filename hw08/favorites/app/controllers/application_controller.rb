3333f12be9e9ca34e1c5a8101467f19ae2d8be16
murtazajafferji <murtazajafferji@gmail.com>
2010-06-16 07:33:50 -0400

    resubmitted hw08

---
+++ hw08/favorites/app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  layout "store"
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery :secret => '8fc080370e56e929a2d5afca5540a0f7'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+  
+  # for authlogic
+  filter_parameter_logging :password
+  helper_method :current_user
+
+private
+  
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find  
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+
+end
