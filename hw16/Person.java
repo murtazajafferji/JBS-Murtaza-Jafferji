de7824d21bd277d217dbb2effa47fd3bd664ab31
murtazajafferji <murtazajafferji@gmail.com>
2010-07-06 14:02:27 -0400

    hw16

---
+++ hw16/Person.java
@@ -0,0 +1,11 @@
+	public class Person{
+		public String fname;
+		public String lname;
+		public String email;
+ 
+		public Person(String fname, String lname, String email){
+			this.fname = fname;
+			this.lname = lname;
+			this.email = email;
+		}
+	}
